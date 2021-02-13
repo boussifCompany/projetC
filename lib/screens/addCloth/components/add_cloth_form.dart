89a6dde8021743ac58d8ec11107673f35bc531aa
unknown <eexdhe@gmail.com>
2021-02-13 16:27:46 +0100

    add image picker

--- lib/screens/addCloth/components/add_cloth_form.dart
+++ lib/screens/addCloth/components/add_cloth_form.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:clothis/models/cloth_types.dart';
 import 'package:clothis/models/colors_list.dart';
 import 'package:clothis/styles/style.dart';
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
 
 class AddClothForm extends StatefulWidget {
   var callback;
@@ -16,6 +21,7 @@ class AddClothForm extends StatefulWidget {
 
 class _AddClothFormState extends State<AddClothForm> {
   final _formkey = GlobalKey<FormState>();
+  File image = null;
   String _brand = "";
   String _type = EnumToString.convertToString(ClothTypes.HOODIE);
   String _color = EnumToString.convertToString(ColorsList.BLACK);
@@ -27,6 +33,19 @@ class _AddClothFormState extends State<AddClothForm> {
       form.save();
       widget.callback(_type, _color, _brand);
     }
+  }
+
+  Future<void> _takePicture() async {
+    final picker = new ImagePicker();
+    final file = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      image = File(file.path);
+    });
+
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final savedImage = await image.copy('${appDir.path}/${fileName}');
   }
 
   @override
@@ -43,6 +62,14 @@ class _AddClothFormState extends State<AddClothForm> {
                 textAlign: TextAlign.center,
               ),
             ),
+            Container(
+              child: (image != null) ?
+                  Image.file(image): null,
+            ),
+            RaisedButton(
+                child: Padding(
+                    padding: EdgeInsets.all(10), child: Text('Take a picture')),
+                onPressed: _takePicture),
             DropdownButtonFormField(
               icon: Icon(Icons.arrow_downward),
               iconSize: 20,
@@ -55,8 +82,8 @@ class _AddClothFormState extends State<AddClothForm> {
               },
               onSaved: (value) => _type = value.trim(),
               style: TextStyle(color: primaryColor),
-              items:
-                  EnumToString.toList(ClothTypes.values).map<DropdownMenuItem<String>>((String value) {
+              items: EnumToString.toList(ClothTypes.values)
+                  .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -75,8 +102,8 @@ class _AddClothFormState extends State<AddClothForm> {
               },
               onSaved: (value) => _color = value.trim(),
               style: TextStyle(color: primaryColor),
-              items:
-              EnumToString.toList(ColorsList.values).map<DropdownMenuItem<String>>((String value) {
+              items: EnumToString.toList(ColorsList.values)
+                  .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
